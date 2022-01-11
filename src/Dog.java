ef1e7f6a62a9d1822ff8e55ee3cd93edbef5578e
cross <crosstown11223@yahoo.com>
2022-01-11 13:16:58 -0500

    added new code cat to dog

--- src/Dog.java
+++ src/Dog.java
@@ -2,5 +2,8 @@ public class Dog {
 
     public static void main(String[] args) {
         System.out.println("Dog");
+
+        // added  new code  add committed
+        System.out.println("Cat");
     }
 }
